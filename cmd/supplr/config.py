6a382cb74536be338180fc9cc1d38a9165bba6d9
Vladislav Sharov <sharov271@jinr.ru>
2022-08-04 09:44:21 +0300

    Update cmd/supplr/config.py

--- cmd/supplr/config.py
+++ cmd/supplr/config.py
@@ -3,5 +3,5 @@ import os
 # Server address
 IP = "server_address:port"
 
-WORK_DIR =  os.getenv("CANOPEN_DIR")
+WORK_DIR =  os.getenv("SUPPLR_CANOPEN_DIR")
 PATH_YAML = WORK_DIR + "/supplr_canopen/config.yaml"
